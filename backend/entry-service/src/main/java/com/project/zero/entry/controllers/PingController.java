499435bf37264b1b37f931245656d7d1c0fab11a
Rahil Rehan <rahil@rainrelay.com>
2025-09-17 22:11:43 +0530

    Adding gradle setup with monorepo and ping controller for testing

---
+++ backend/entry-service/src/main/java/com/project/zero/entry/controllers/PingController.java
@@ -0,0 +1,13 @@
+package com.project.zero.entry.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Ping from Entry Service!";
+    }
+}
